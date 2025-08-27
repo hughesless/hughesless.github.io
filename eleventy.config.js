4c966e45fafc884ae3b713516374bc2ad47b7ab8
Michael Hughes <mikebhughes@gmail.com>
2025-08-27 11:47:36 -0400

    initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,6 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
+export default async function(eleventyConfig) {
+	// Configure Eleventy
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+};
